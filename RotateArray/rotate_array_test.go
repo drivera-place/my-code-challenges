2c5aee32c7ac22f16943bad3a873d8bfeec08d5c
Dante Rivera <drivera-place@users.noreply.github.com>
2024-03-11 22:46:14 -0600

    added comments

--- RotateArray/rotate_array_test.go
+++ RotateArray/rotate_array_test.go
@@ -5,16 +5,19 @@ import (
 )
 
 func TestRotateArray(t *testing.T) {
+	// Arrange
 	test := []int{1, 2, 3, 4, 5}
 	k := 2
 	expected := []int{4, 5, 1, 2, 3}
 
+	// Act
 	output := rotateArray(test, k)
 
-	compare(expected, output, t)
+	// Assert
+	assert(expected, output, t)
 }
 
-func compare(expected, output []int, t *testing.T) {
+func assert(expected, output []int, t *testing.T) {
 
 	if len(expected) != len(output) {
 		t.Fatalf(`Arrays length is not equal: expected: %v, output %v:`,expected, output)
